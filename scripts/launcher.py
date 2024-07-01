9debdbc727c9c7ad6266d0d60aba05d4e7d5a71f
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-07-01 12:34:19 -0700

    Fix config directory path

--- scripts/launcher.py
+++ scripts/launcher.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
         task_logic_schema=AindVideoEncodingBenchmarksTaskLogic,
         data_dir=r"C:/Data",
         remote_data_dir=r"\\allen\aind\scratch\video-encoding-benchmarks\data",
-        config_library_dir=r"\\allen\aind\scratch\AindBehavior.db\AindVideoEncodingBenchmarksRig",
+        config_library_dir=r"\\allen\aind\scratch\AindBehavior.db\AindVideoEncodingBenchmarks",
         workflow=r"./src/main.bonsai",
     )
     launcher_cli.run()
